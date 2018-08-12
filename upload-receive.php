bf3d1ca9361ad63ade3dd48ec919395fd1e3913c
martin <martin@luckycloud.de>
2018-08-12 20:22:50 +0200

    Add upload hook from images sent via WiFi per Arducam / ESP8266

---
+++ upload-receive.php
@@ -0,0 +1,62 @@
+<?php
+$base = "./";
+if ($_FILES["upload"]["error"] > 0)
+{
+    throw new UploadException($_FILES['upload']['error']);
+}
+else {
+    $fileName = $_FILES["upload"]["name"];
+    $explodeFile = explode(".", $fileName);
+    $extension = end($explodeFile);
+    $directoryDate = $base . date('Y-m-d') . "/";
+    $uploadedName = date('l-H-i-s').".".$extension;
+// Check if directory exists if not create it
+
+        if (!is_dir($directoryDate)) {
+            mkdir($directoryDate);
+        }
+        move_uploaded_file($_FILES["upload"]["tmp_name"], $directoryDate.$uploadedName);
+        echo "<img src=\"".$directoryDate.$uploadedName."\">";
+
+}
+
+
+class UploadException extends Exception
+{
+    public function __construct($code) {
+        $message = $this->codeToMessage($code);
+        parent::__construct($message, $code);
+    }
+
+    private function codeToMessage($code)
+    {
+        switch ($code) {
+            case UPLOAD_ERR_INI_SIZE:
+                $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+                break;
+            case UPLOAD_ERR_FORM_SIZE:
+                $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                $message = "The uploaded file was only partially uploaded";
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                $message = "No file was uploaded";
+                break;
+            case UPLOAD_ERR_NO_TMP_DIR:
+                $message = "Missing a temporary folder";
+                break;
+            case UPLOAD_ERR_CANT_WRITE:
+                $message = "Failed to write file to disk";
+                break;
+            case UPLOAD_ERR_EXTENSION:
+                $message = "File upload stopped by extension";
+                break;
+
+            default:
+                $message = "Unknown upload error";
+                break;
+        }
+        return $message;
+    }
+}
