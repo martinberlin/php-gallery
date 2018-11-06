271a83a4780f410e787fbb0b0f59899f4efbe9ee
martinberlin <martin@luckycloud.de>
2018-11-06 09:09:39 +0100

    Pending process XBM to json

--- upload-xbm.php
+++ upload-xbm.php
@@ -1,57 +1,72 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: martin
- * Date: 04.11.18
- * Time: 20:23
- */
+require("uploadClass.php");
+$getFolder = isset($_GET['f']) ? $_GET['f'] : 'no_folder';
+$clientFolder = "{$getFolder}/";
+
+// CONFIG
+$thumb = array();
+$thumb['width']  = 128;
+$thumb['height'] = 64;
+$directoryBase = "camera-uploads/";
+$uploadBase = "uploads/".$clientFolder;
+
+if ($_FILES["upload"]["error"] > 0)
+{
+    throw new UploadException($_FILES['upload']['error']);
+} else {
+    $fileName = $_FILES["upload"]["name"];
+    $explodeFile = explode(".", $fileName);
+    $extension = end($explodeFile);
+    $directoryDate = $uploadBase . date('Y-m-d') . "/";
+    $uploadedName = date('l-H-i-s').".".$extension;
+
+// Check if directory exists if not create it
+    if (!is_dir($directoryDate)) {
+        //exit($directoryDate);
+        mkdir($directoryDate);
+    }
+    $uploadedFile = $directoryDate.$uploadedName;
+    $uploaded = move_uploaded_file($_FILES["upload"]["tmp_name"], $uploadedFile);
+
+}
+
+if ($uploaded == false) {
+    $imageLink = "http://".$_SERVER['HTTP_HOST']."/".$directoryBase."gallery/assets/error-uploading.png";
+    exit($imageLink);
+}
+$imageObj = array();
+
+
+
 // Create a blank image and add some text
 //$im = imagecreatetruecolor(128, 64);
-$im = imagecreatefromjpeg('xbm/3.jpg');
-$imgw = imagesx($im);
-$imgh = imagesy($im);
-for ($i=0; $i<$imgw; $i++)
-{
-    for ($j=0; $j<$imgh; $j++)
-    {
+//$im = imagecreatefromjpeg('xbm/3.jpg');
+$im = new \Imagick(realpath($uploadedFile));
+$im->setCompressionQuality(99);
+$im->resizeImage($thumb['width'], $thumb['height'], Imagick::FILTER_LANCZOS, 1, 0);
 
-        // Get the RGB value for current pixel
+$im->quantizeImage(8,                        // Number of colors  8  (8/16 for depth 4)
+    Imagick::COLORSPACE_GRAY, // Colorspace
+    1,                        // Depth tree  16
+    TRUE,                     // Dither
+    FALSE);
 
-        $rgb = imagecolorat($im, $i, $j);
-
-        // Extract each value for: R, G, B
-
-        $rr = ($rgb >> 16) & 0xFF;
-        $gg = ($rgb >> 8) & 0xFF;
-        $bb = $rgb & 0xFF;
-
-        // Get the value from the RGB value
-
-        $g = round(($rr + $gg + $bb) / 3);
-
-        // Gray-scale values have: R=G=B=G
-
-        //$g = (r + g + b) / 3
-        if($g> 0x7F) //you can also use 0x3F 0x4F 0x5F 0x6F its on you
-            $g=0xFF;
-        else
-            $g=0x00;
-
-
-
-        $val = imagecolorallocate($im, $g, $g, $g);
-
-        // Set the gray value
-
-        imagesetpixel ($im, $i, $j, $val);
-    }
-}
 /** Alternative:
 imagefilter($img, IMG_FILTER_GRAYSCALE); //first, convert to grayscale
 imagefilter($img, IMG_FILTER_CONTRAST, -255); //then, apply a full contrast
 imagejpeg($img);
  */
 // Save the image
-imagexbm($im, 'xbm/3.xbm');
+$im->setImageFormat('xbm'); //xbm
+$im->writeimage("xbm/test.xbm");
+// Populate imageObj before json encoding
+// TODO Process imageBlob
+$imageObj['xbm'] = $im->getImageBlob();
 
-imagedestroy($im);
+$imageObj['thumb'] = array(
+    'width' => $thumb['width'],
+    'height' => $thumb['height']
+    );
+
+$imageObj['url'] = "http://".$_SERVER['HTTP_HOST']."/".$directoryBase."gallery/assets/error-uploading.png";
+echo json_encode($imageObj);
